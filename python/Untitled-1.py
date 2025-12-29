f4c2ffa3448f07447861c112d17335e533335dd1
dyaplo <g76665954@gmail.com>
2025-12-28 18:25:44 -0800

    frist Calculator

---
+++ python/Untitled-1.py
@@ -0,0 +1,69 @@
+num1 = float(input("enter number : "))
+operator=input()
+num2 = float(input("enter number"))
+
+if operator == "+" :
+    print(num1 + num2)
+
+elif operator == "-" :
+    print(num1 - num2)
+
+elif operator == "*" :
+    print(num1 * num2)
+
+elif operator == "/" :
+    print(num1 / num2)
+
+elif operator == "^" :
+    print(num1 ** num2)
+
+else:
+    print("error")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
